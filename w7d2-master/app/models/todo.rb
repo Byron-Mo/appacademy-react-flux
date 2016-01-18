35ad6c00c95ed43f713d059b36858dbe0dca552a
Byron Mo <byronmo@gmail.com>
2016-01-17 20:41:20 -0800

    update w7d3 project

--- w7d2-master/app/models/todo.rb
+++
@@ -1,4 +0,0 @@
-class Todo < ActiveRecord::Base
-  validates :title, presence: true
-  validates :done, inclusion: { in: [true, false]}
-end
--- w7d2-master/config/routes.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.routes.draw do
-  root to: 'static_pages#root'
-
-  namespace :api do
-    resources :todos, except: [:new, :edit]
-  end
-end
